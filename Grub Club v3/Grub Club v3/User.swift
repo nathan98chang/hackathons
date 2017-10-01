f46f53e6f22778f4f3f1773393fafb031519c3d0
Nathan Chang <changnat@usc.edu>
2017-09-30 22:01:01 -0700

    Add files via upload
    HackMerced iOS App files

---
+++ Grub Club v3/Grub Club v3/User.swift
@@ -0,0 +1,35 @@
+//
+//  User.swift
+//  Grub Club v3
+//
+//  Created by Brendan Wong on 9/30/17.
+//  Copyright © 2017 Nathan Chang. All rights reserved.
+//
+
+import UIKit
+
+class User {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    //add whatever vars you need here, but be sure to update the initializer
+    
+
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        self.name = name
+        self.photo = photo
+    }
+    
+    
+}
